6558a803c7e4bcfc0a8be8ed7bf9fe1b0bda13fe
yisroel96 <yisroel96@gmail.com>
2019-07-17 03:46:17 -0400

    created postgres database and migrated datatbase'

--- db/migrate/20190717071320_create_appointments.rb
+++ db/migrate/20190717071320_create_appointments.rb
@@ -1,6 +1,9 @@
 class CreateAppointments < ActiveRecord::Migration[5.2]
   def change
     create_table :appointments do |t|
+      t.date :date
+      t.string :time
+      t.integer :client_id
 
       t.timestamps
     end
