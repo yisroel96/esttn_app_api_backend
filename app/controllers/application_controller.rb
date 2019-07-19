ac1fff8fa8fdc3a96d31609be5c5c266b73e1f6d
yisroel96 <yisroel96@gmail.com>
2019-07-18 20:06:07 -0400

    added mock login helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    helpers do
+        def current_le
+            Le.first
+        end
+
+        def logged_in?
+            !!current_le
+        end
+    end
 end
