94ba8f3d1f9742d8b88370be02bf3d90f3571b76
yisroel96 <yisroel96@gmail.com>
2019-07-18 20:11:43 -0400

    fixed mock login methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
-    helpers do
-        def current_le
-            Le.first
-        end
 
-        def logged_in?
-            !!current_le
-        end
+    def current_le
+        Le.first
     end
+
+    def logged_in?
+        !!current_le
+    end
+    
 end
