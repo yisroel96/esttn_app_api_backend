4e0a53660f62acb458883cdd4ec6d47dc9a888db
yisroel96 <yisroel96@gmail.com>
2019-07-24 02:06:57 -0400

    created product strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,14 @@
 class ProductsController < ApplicationController
 
     def create
-        @product = Product.create(name: params[:product][:name], le_id: params[:product][:le_id])
+        # @product = Product.create(name: params[:product][:name], le_id: params[:product][:le_id])
+        @product = Product.create(product_params)
         render json: @product
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :le_id)
     end
 end
